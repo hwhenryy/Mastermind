4a858e74a42ebd203236874a869ac33e90606c08
paulvonoberstein <78957609+paulvonoberstein@users.noreply.github.com>
2022-01-20 15:27:04 +1300

    added delete and confirm buttons

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 var messageEl = document.querySelector("#message-el");
-var isAlive = false;
+var lives = 0;
 var difficultySelect = false;
 
 
@@ -16,6 +16,36 @@ function startGame() {
 
     // Replace message to prompt for number of digits
     messageEl.textContent = "Select the number of digits you wish to guess: ";
+
+    // Add the display for numbers
+    var tag = document.createElement("p");
+    var text = document.createTextNode("test");
+    tag.appendChild(text);
+    var element = document.getElementById("new");
+    element.appendChild(tag);
     
 
-}
+    // Render the buttons with a for loop
+    for (var i = 1; i <10; i++) {
+        var numberBtn = document.createElement("button")
+        numberBtn.innerHTML =  i;
+        numberBtn.id = i + "-btn";
+        document.getElementById("number-buttons").appendChild(numberBtn);
+    }
+
+    // add a delete and confirm button
+
+        var deleteBtn = document.createElement("button")
+        deleteBtn.innerHTML =  "DELETE";
+        deleteBtn.id = "delete-btn";
+        document.getElementById("DC-buttons").appendChild(deleteBtn);
+
+
+        var confirmBtn = document.createElement("button")
+        confirmBtn.innerHTML =  "CONFIRM";
+        confirmBtn.id = "confirm-btn";
+        document.getElementById("DC-buttons").appendChild(confirmBtn);
+
+    // add a confirm button
+} 
+
